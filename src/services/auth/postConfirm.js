052ce37e2d98fc7c046353ba04ab485333fe0e81
Michael Liu <michael.liu0@gmail.com>
2019-06-18 07:40:06 -0700

    Nighly June 17

--- src/services/auth/postConfirm.js
+++ src/services/auth/postConfirm.js
@@ -1,6 +1,33 @@
+const middy = require( 'middy' );
+const { ssm } = require( 'middy/middlewares' );
 
+const faunadb = require( 'faunadb' );
 
-module.exports.main = async (event) => {
-    console.log( event.request );
+const handler = async (event) => {
+
+    // Check the trigger source
+    if( event.triggerSource === 'PostConfirmation_ConfirmSignUp' ){
+
+        // If this source came from confirming sign up
+
+        const key = process.env.faunaKey;
+
+        console.log( key );
+
+        // Make a new user
+
+        
+    }
+
     return event;
 };
+
+module.exports.main = middy( handler ).use(
+    // Using SSM middleware, will cache and decrypt for us
+    ssm({
+        cache: true,
+        names: {
+            faunaKey: 'FaunaKey'
+        }
+    })
+);
