abef98e02bc7909a88fe2d3ba1eb8e5d516a4b5b
Michael Liu <michael.liu0@gmail.com>
2019-06-22 22:00:51 -0700

    Testing for Users API underway. Currently SSM module is being included, but the directory doesn't line up in Lambda, so requires don't work in the cloud.
    Thinking of putting SSM in Layer.

--- src/services/API/users/users.js
+++ src/services/API/users/users.js
@@ -1,21 +1,45 @@
 'use strict';
 
+const ssmModule = require( './util/ssm' ); // Import the SSM module
+const ssm = new ssmModule( ["FaunaKey"] ); // Spin up a new instance with our list of keys
+
 const faunadb = require( 'faunadb' );
 const q = faunadb.query;
-const client = new faunadb.Client({ secret: 'fnADRDqMRAACCXjBou0jxtqZPElE-8Dl-yIYPst7' });
+let client;
+
 
 module.exports.main = async ( event ) => {
 
-    const res = await client.query(
-        q.Create(
-            q.Class( "users" ),
-            { data: { id: 'c71607d2-6682-41b9-bf04-144973433b39', displayName: q.NewId() } }
-        )
-    );
+    // Make the client
+    if( !client ){
+        // Fetch the DB key
+        const dbKey = await ssm.getParam( 'FaunaKey' );
 
-    console.log( res );
+        // Create a new client
+        client = new faunadb.Client({ secret: dbKey });
+    }
 
-    return null;
+    const displayName = event.pathParameters.displayName;
+
+    let result;
+
+    try{
+
+        result = await client.query(
+            q.Get(
+                q.Match(
+                    q.Index( "user_by_displayName"),
+                    displayName
+                )
+            )
+        );
+
+    } catch( e ){
+        console.error( e.message );
+        console.error( "Error was for display name: " + displayName );
+        result = 'None';
+    }
+
 
     const headers = {
         "Access-Control-Allow-Origin": "*"
@@ -25,8 +49,8 @@ module.exports.main = async ( event ) => {
         statusCode: 200,
         headers: headers,
         body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            event: event
+            message: 'The result is in',
+            result: result
         }, null, 2),
     };
 
