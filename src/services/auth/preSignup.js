a21fdb24348d1782337a8ed95d479d01361b9ef8
Michael Liu <michael.liu0@gmail.com>
2019-08-14 15:22:59 -0700

    Signup related triggers switched over to GraphQL

--- src/services/auth/preSignup.js
+++ src/services/auth/preSignup.js
@@ -1,7 +1,42 @@
-const q = require( './faunaGraphqlQuery' );
+const faunaQuery = require( './faunaGraphqlQuery' );
 
 module.exports.main = async (event) => {
 
-    // TODO do stuff
+    // Don't care if we're handling Admin singup
+    if( event.triggerSource !== 'PreSignUp_SignUp' ){
+        // Return the event and end the lambda
+        return event;
+    }
+
+    // Otherwise
+
+    const sub = event.userName;
+    const displayName = event.request.userAttributes.name;
+
+    // Make the user
+    const res = await faunaQuery(`
+        mutation MakeUser{
+            createUser( data: {
+                sub: "${sub}"
+                displayName: "${displayName}"
+                type: FREE
+                active: false
+            }){
+                _id
+                sub
+                displayName
+            }
+        }
+    `);
+
+    if( res.errors ){
+        console.error( `Error creating user, Error message: ${ res.errors }` );
+        throw new Error( `Error creating user. Error Message: ` + JSON.stringify( res.errors ) )
+    }
+
+    // DEBUG for convenience
+    // event.response.autoConfirmUser = true;
+
+    return event;
 
 };
--- src/util/faunaGraphqlQuery.js
+++ src/util/faunaGraphqlQuery.js
@@ -3,7 +3,7 @@ const faunaKeyName = `/fauna/FaunaKey-${process.env.STAGE}`;
 
 const ssm = new ssmModule( [faunaKeyName] ); // Spin up a new instance with our list of keys
 
-const btoa = require( 'btoa' );
+const fetch = require( 'node-fetch' );
 
 module.exports = async ( query ) => {
 
@@ -13,7 +13,7 @@ module.exports = async ( query ) => {
     const res = await fetch( 'https://graphql.fauna.com/graphql', {
         method: 'POST',
         headers: {
-            'Authorization': 'Bearer ' + btoa( key )
+            'Authorization': 'Bearer ' + key
         },
         // MUST STRINGIFY BODY, THIS IS IN GRAPHQL SPECS
         body: JSON.stringify( {
